e3655d763e4fe0a4eef0446474dd146d5be6e9ef
clockj <szhong12@jhu.edu>
2024-01-02 16:43:23 -0500

    update

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ cd release
 rmdir /s release-win
 md release-windows_64x
 cd release-windows_64x
-python -m PyInstaller -F --collect-submodules=pydicom --noconsole --onefile --windowed ../../main.py --name OpenLPTGUI --icon ../../icons/Clock.ico
+python -m PyInstaller -F --collect-submodules=pydicom --noconsole --onefile --windowed ../../main.py --name OpenLPTGUI --icon ../../icons/RuiLab.ico
 cd dist
 Xcopy .\..\..\..\icons .\icons /E /H /C /I
 Xcopy .\..\..\..\fonts .\fonts /E /H /C /I
